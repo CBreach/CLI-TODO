e48a9a25a845e1bc443d771bd0174cf61f802580
CBreach <cbreach@sandiego.edu>
2025-06-05 23:05:49 -0700

    initialized cobra for command usage and initialized the basis of a rudimentary add command

---
+++ cmd/add.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "adds a new task to your list of tasks",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) <= 0 {
+			fmt.Println("task parameter required")
+			return
+		}
+		fmt.Println("item:", strings.Join(args, " "), "added to the list.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
